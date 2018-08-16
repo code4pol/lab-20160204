f9fa21bbbcb339071269b2c4c491d9538a69372c
Alexandre Gomes <alegomes@gmail.com>
2018-08-16 08:24:31 -0300

    bkp

--- coletadetweetscomstream.py
+++ coletadetweetscomstream.py
@@ -10,10 +10,15 @@ class MyStreamListener(tweepy.StreamListener):
 		print("\n",data)
 		# print("%d,\"%s\"" % (data.id,data.text))
 
-consumer_key="suachave"
-consumer_secret="suachave"
-access_token="suachave"
-access_token_secret="suachave"
+# consumer_key="suachave"
+# consumer_secret="suachave"
+# access_token="suachave"
+# access_token_secret="suachave"
+
+consumer_key="BZcGMGJTDGrpdVcZioyy2XeXQ"
+consumer_secret="VRHcRLtGcKwVulTnfU7q9X2qDY006IOWBJOvvPjs23X2Ovocv6"
+access_token="14147108-ooP2hAhZPXEOX6j6HoqE4sf3lg9nQ7Rke6fiOvHEx"
+access_token_secret="I7WHX4FyNTuZ0MjPmoXyTHQFkFlcI0Eb4jbYG5cgR2YdV"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
